cdd694e57f017489e85d19a1b8e2bd407851b846
Hamza <hamzamagdy0@gmail.com>
2024-10-18 17:17:35 +0300

    Added Job lister and Job Seeker components

---
+++ src/app/admins/job-lister-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-lister-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-lister-view.component.html',
+  styleUrl: './job-lister-view.component.css'
+})
+export class JobListerViewComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 
-//Main Pages Componanets
+// Main Pages Components
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UsersComponent } from './users/users.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { AdminsComponent } from './admins/admins.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { Error404Component } from './error404/error404.component';
+import { JobListerViewComponent } from './admins/job-lister-view.component';
+import { JobSeekerViewComponent } from './admins/job-seeker-view.component';
 
-//Routers
+// Routers
 import { userRoutes } from './users/users.routes';
 import { companyRoutes } from './companies/companies.routes';
 import { adminRoutes } from './admins/admins.routes';
@@ -32,6 +34,16 @@ export const routes: Routes = [
         path: 'admin',
         children: adminRoutes,
         component: AdminsComponent
+    },
+    {
+        path: 'job-lister',
+        component: JobListerViewComponent,
+        title: 'Job Lister View'  // Title for the job lister view
+    },
+    {
+        path: 'job-seeker',
+        component: JobSeekerViewComponent,
+        title: 'Job Seeker View'  // Title for the job seeker view
     },
     {
         path: 'unauthorized',
