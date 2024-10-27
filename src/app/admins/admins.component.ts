19e21b54774a1eba6140f25dd0e38e9638177086
AhmedH2004 <duduhesham2004.ah.ah@gmail.com>
2024-10-27 10:33:56 +0300

    Latest code

--- src/app/admins/admins.component.ts
+++ src/app/admins/admins.component.ts
@@ -1,45 +1,11 @@
 import { Component } from '@angular/core';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-admins',
   standalone: true,
-  imports: [],
+  imports: [AdminNavComponent,RouterOutlet],
   templateUrl: './admins.component.html',
   styleUrls: ['./admins.component.css']
 })
-export class AdminsComponent {
-  
-  navToggle(e: any) {
-    // Get all SVG elements with the class 'personIcon' and 'companyIcon' in the container
-    const allSvgIcons = document.querySelectorAll('.personIcon, .companyIcon');
-    
-    // Reset all icons to the original fill and background color
-    allSvgIcons.forEach((icon: any) => {
-      const path = icon.querySelector('path'); // The path inside the SVG
-      path.setAttribute('fill', '#dedada'); // Reset to original icon color
-      icon.style.backgroundColor = 'transparent'; // Reset background color
-    });
-  
-    // Apply the new color to the clicked icon
-    const svgIcon = e.currentTarget; // The whole SVG element
-    const path = svgIcon.querySelector('path'); // The path inside the SVG
-    
-    // Change the clicked icon color
-    path.setAttribute('fill', '#2F4157'); // Dark blue color for the icon
-    
-    // Apply background color to the clicked SVG container
-    svgIcon.style.backgroundColor = '#DADDD9'; // Light background color
-
-    // Find the title element
-    const titleElement : any = document.querySelector('.title');
-
-    // Check the class of the clicked icon and update the title text accordingly
-    if (svgIcon.classList.contains('companyIcon')) {
-        console.log("Job listers view");
-        titleElement.textContent = "Job Listers View"; // Update the title text
-    } else if (svgIcon.classList.contains('personIcon')) {
-        console.log("Job seekers view");
-        titleElement.textContent = "Job Seekers View"; // Update the title text
-    }
-  }
-}
+export class AdminsComponent {}
