03bdb6cf05c9badfa001c22eca4ce256c9a64611
YousefAli-1 <yousefalisaied@gmail.com>
2024-11-26 16:28:06 +0200

    .

--- src/app/users/find-a-job/filters/filters.component.ts
+++ src/app/users/find-a-job/filters/filters.component.ts
@@ -10,46 +10,8 @@ import { CareerLevelType } from '../../../app.model';
 })
 export class FiltersComponent {
   workplace= true;
-   isExperienceDisplayed= true;
-   isLocationDisplayed= true;
-   // Available options for each section (you can later fetch these from an API)
-   workplaceOptions = [
-      'Remote',
-      'On-site',
-      'Hybrid'
-   ];
- 
-   experienceOptions = [
-     'entry-level',
-     'expertised-level',
-     'senior-manager',
-     'Internship'
-   ];
- 
-   locationOptions = [
-     'Cairo',
-     'Alexandria',
-     'Giza',
-     'Sharm El Sheikh',
-     'Hurghada',
-     'Aswan'
-   ];
-
-   // Toggle display for each section
-   toggleDisplay(section: string): void {
-     switch(section) {
-       case 'workplace':
-         this.workplace = !this.workplace;
-         break;
-       case 'experience':
-         this.isExperienceDisplayed = !this.isExperienceDisplayed;
-         break;
-       case 'location':
-         this.isLocationDisplayed = !this.isLocationDisplayed;
-         break;
-     }
-   }
-
+  experience= true;
+  location= true;
    openCategoryDropdown(categoryId: string) {
     const categoryDropdown = document.getElementById(categoryId);
     categoryDropdown?.click();
@@ -86,5 +48,9 @@ export class FiltersComponent {
     'experience': []
   };
 
+  data: string | null = null;
 
+  toggleDisplay(groupName: string) {
+    this.data = this.data === groupName ? null : groupName;
+  }
 }
