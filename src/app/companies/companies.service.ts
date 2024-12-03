549d23c5f5d4fdeddb97375af05132ec70002ad0
MostafaAyman-14 <149167922+MostafaAyman-14@users.noreply.github.com>
2024-12-03 18:52:11 +0200

    Add files via upload

--- src/app/companies/companies.service.ts
+++ src/app/companies/companies.service.ts
@@ -1,39 +1,48 @@
 import { Injectable } from '@angular/core';
-import { posts } from '../app.model';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
+import { posts } from '../app.model';  
+
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
-  private jobPosts: posts[] = [];
-  private storageKey = 'jobPosts';
-  
-  private jobPostsSubject = new BehaviorSubject<any[]>(this.loadPosts());
-  jobPosts$ = this.jobPostsSubject.asObservable();
+  private jobPostsSubject = new BehaviorSubject<posts[]>([]); 
+  jobPosts$ = this.jobPostsSubject.asObservable();  
+  private apiUrl = 'http://localhost:8080/company';  
 
-  private loadPosts(): any[] {
-    return JSON.parse(localStorage.getItem('jobPosts') || '[]');
-  }
-  addPost(post: any) {
-    const existingPosts = this.loadPosts();
-    existingPosts.push(post);
-    localStorage.setItem('jobPosts', JSON.stringify(existingPosts));
-    this.jobPostsSubject.next(existingPosts); 
-  }
-  editPost(updatedPost: any, index: number) {
-    const existingPosts = this.loadPosts();
-    existingPosts[index] = updatedPost; 
-    localStorage.setItem(this.storageKey, JSON.stringify(existingPosts));
-    this.jobPostsSubject.next(existingPosts); 
+  constructor(private http: HttpClient) {
+    this.loadJobPosts();
   }
 
-  deletePost(index: number) {
-    const existingPosts = this.loadPosts();
-    existingPosts.splice(index, 1); 
-    localStorage.setItem(this.storageKey, JSON.stringify(existingPosts));
-    this.jobPostsSubject.next(existingPosts);
+  // Load job posts from the backend
+  loadJobPosts() {
+    this.http.get<posts[]>(`${this.apiUrl}/getPosts`).subscribe(posts => {
+      this.jobPostsSubject.next(posts);
+    });
   }
-  getJobPosts(): posts[] {
-    return this.jobPosts;
+
+  // Method to add a new job post
+  addPost(newPost: posts) {
+    this.http.post<posts>(`${this.apiUrl}/addPost`, newPost).subscribe(post => {
+      const updatedPosts = [...this.jobPostsSubject.getValue(), post];
+      this.jobPostsSubject.next(updatedPosts); 
+    });
+  }
+
+  // Method to delete a job post by id
+  deletePost(id: string) {
+    this.http.delete(`${this.apiUrl}/deletePost/${id}`).subscribe(() => {
+      const updatedPosts = this.jobPostsSubject.getValue().filter(post => post.id !== id);
+      this.jobPostsSubject.next(updatedPosts);
+    });
+  }
+   // Edit an existing job post by ID
+   editPost(updatedPost: posts, id: string) {
+    this.http.put<posts>(`${this.apiUrl}editPost/${id}`, updatedPost).subscribe((post) => {
+      const currentPosts = this.jobPostsSubject.getValue();
+      const updatedPosts = currentPosts.map((p) => (p.id === id ? post : p)); 
+      this.jobPostsSubject.next(updatedPosts);
+    });
   }
 }
--- src/app/companies/post/post.component.ts
+++ src/app/companies/post/post.component.ts
@@ -1,21 +1,20 @@
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators ,ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CompaniesService } from '../companies.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
 export class PostComponent {
   postForm: FormGroup;
-  jobPosts: any[] = [];
   isSubmitted = false;
+
   constructor(
     private fb: FormBuilder,
     private companyService: CompaniesService,
@@ -26,42 +25,37 @@ export class PostComponent {
       careerLevel: ['', Validators.required],
       workplace: ['', Validators.required],
       jobRequirements: ['', Validators.required],
-      jobCategory:['',Validators.required],
-      jobDescription:['']
+      jobCategory: ['', Validators.required],
+      jobDescription: ['',Validators.required],
+      tags:['']
     });
-    this.loadJobPosts();
   }
-  loadJobPosts() {
-    const storedPosts = localStorage.getItem('jobPosts');
-    if (storedPosts) {
-      this.jobPosts = JSON.parse(storedPosts); 
-    }
-  }
+
   submitForm() {
     console.log("Form Data before saving:", this.postForm.value);
-    this.isSubmitted = true; 
+    this.isSubmitted = true;
     if (this.postForm.valid) {
       console.log('Form is valid, submitting data...');
       const newPost = {
+        id: new Date().toISOString(),  
         jobTitle: this.postForm.value.jobTitle,
         careerLevel: this.postForm.value.careerLevel,
-        jobCategory:this.postForm.value.jobCategory,
+        jobCategory: this.postForm.value.jobCategory,
         workplace: this.postForm.value.workplace,
         jobDescription: this.postForm.value.jobDescription,
-        jobRequirements:this.postForm.value.jobRequirements
+        jobRequirements: this.postForm.value.jobRequirements,
+        tags:this.postForm.value.tags
       };
-      const existingPosts = JSON.parse(localStorage.getItem('jobPosts') || '[]');
-      localStorage.setItem('jobPosts', JSON.stringify(existingPosts));
-      console.log("Posts after saving:", existingPosts); 
+      this.companyService.addPost(newPost);
+      console.log(newPost);
       this.postForm.reset();
-      this.companyService.addPost(newPost); 
-      console.log('New Job Post:', newPost);
-      console.log(localStorage.getItem('jobPosts'));
-      this.router.navigate(['company/post/posts']);  
+      this.router.navigate(['company/post/posts']).then(() => {
+        console.log('Navigation successful!');
+      });
+       
     } else {
       console.log('Form is invalid');
-      localStorage.clear();
-      this.postForm.markAllAsTouched();  
+      this.postForm.markAllAsTouched();
     }
   }
 }
